7337073428d85668292a0a4eaef74ef493d9c316
yanglet <y2yone023@naver.com>
2022-05-20 16:03:32 +0900

    feat: 백엔드 프로젝트 세팅

---
+++ backend/module-application/src/main/java/com/project/cs/CryptocurrencySimulatorApplication.java
@@ -0,0 +1,11 @@
+package com.project.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencySimulatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CryptocurrencySimulatorApplication.class, args);
+    }
+}
---
+++ backend/module-common/src/main/java/com/project/cs/test/test.java
@@ -0,0 +1,8 @@
+package com.project.cs.test;
+
+import lombok.Data;
+
+@Data
+public class test {
+    private String test;
+}
