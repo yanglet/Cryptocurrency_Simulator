eff3c5152d2cd2f346d2a8d92a54f5e4f8cc28d1
yanglet <y2yone023@naver.com>
2022-05-28 02:04:46 +0900

    feat: swagger 적용

--- backend/module-application/src/main/java/com/project/cs/cryptocurrency/controller/CryptocurrencyController.java
+++ backend/module-application/src/main/java/com/project/cs/cryptocurrency/controller/CryptocurrencyController.java
@@ -2,9 +2,12 @@ package com.project.cs.cryptocurrency.controller;
 
 import com.project.cs.cryptocurrency.dto.CryptocurrencyDto;
 import com.project.cs.cryptocurrency.repository.CryptocurrencyRepository;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +19,9 @@ public class CryptocurrencyController {
 
     private final CryptocurrencyRepository cryptocurrencyRepository;
 
+    @ApiOperation("모든 가상화폐 조회")
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<CryptocurrencyDto> getCryptocurrencies(){
         return cryptocurrencyRepository.findAll();
     }
