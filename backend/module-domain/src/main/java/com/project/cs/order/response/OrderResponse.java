799fe7d3f5ed60ac682ab35e6c4e90e3e04d39a2
yanglet <y2yone023@naver.com>
2022-06-27 01:55:05 +0900

    feat: 주문에 대한 request, response 생성

---
+++ backend/module-domain/src/main/java/com/project/cs/order/response/OrderResponse.java
@@ -0,0 +1,10 @@
+package com.project.cs.order.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class OrderResponse {
+    private Long id;
+}
