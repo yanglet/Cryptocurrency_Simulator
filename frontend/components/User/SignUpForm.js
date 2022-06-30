d04e3f3e5cefa75274a8a50399d63a1d1e8f3492
sein <hyuntpdls@gmail.com>
2022-06-30 12:28:24 +0900

    feat: 회원가입 기능 구현

--- frontend/components/User/SignUpForm.js
+++ frontend/components/User/SignUpForm.js
@@ -1,77 +1,108 @@
+import {useRouter} from 'next/router';
+
 import React, { useState, useCallback } from "react";
+import AuthService from "../../services/auth.service";
 import useInput from "../hooks/useInput";
 
 function SignUpForm(props) {
-  const [id, onChangeId] = useInput("");
-  const [nickname, onChangeNickname] = useInput("");
+  const router = useRouter();
+  const [email, onChangeEmail] = useInput("");
+  const [name, onChangeName] = useInput("");
   const [password, onChangePassword] = useInput("");
-  const [passwordCheck, setPasswordCheck] = useState("");
-  const [passwordError, setPasswordError] = useState(false);
+  const [balance, onChangeBalance] = useInput("");
+  // const [passwordCheck, setPasswordCheck] = useState("");
+  // const [passwordError, setPasswordError] = useState(false);
 
-  const onChangePasswordCheck = useCallback(
-    (e) => {
-      setPasswordCheck(e.target.value);
-      setPasswordError(e.target.value !== password);
-    },
-    [password]
-  );
-  const onSubmitForm = useCallback((e) => {
+  // const onChangePasswordCheck = useCallback(
+  //   (e) => {
+  //     setPasswordCheck(e.target.value);
+  //     setPasswordError(e.target.value !== password);
+  //   },
+  //   [password]
+  // );
+
+  const onSubmitForm = async (e) => {
     e.preventDefault();
-    if (password != passwordCheck) {
-      return setPasswordError(true);
+    try {
+      await AuthService.register(balance, email, name, password).then(
+        () => {
+          router.replace('/');
+          //window.location.reload("/");
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } catch (err) {
+      console.log(err);
     }
-    console.log(id, nickname, password);
-  }, [password, passwordCheck]);
+    // if (password != passwordCheck) {
+    //   return setPasswordError(true);
+    // }
+    // console.log(id, nickname, password);
+  };
 
   return (
     <div className="border rounded-lg px-4 py-4 max-w-lg mx-auto">
       <form onSubmit={onSubmitForm}>
         <div className="flex justify-between mb-6">
           <label className="text-gray-700 w-full text-xl" htmlFor="user-id">
-            아이디
+            이메일
           </label>
           <input
             className="text-xl rounded-lg border-2"
-            name="user-id"
-            value={id}
-            onChange={onChangeId}
+            value={email}
+            onChange={onChangeEmail}
             required
           />
         </div>
         <div className="flex justify-between mb-6">
-          <label className="text-gray-700 w-full text-xl" htmlFor="user-nickname">닉네임</label>
-          <input
-           className="text-xl rounded-lg border-2"
-            name="user-nickname"
-            value={nickname}
-            onChange={onChangeNickname}
-            required
-          />
-        </div>
-        <div className="flex justify-between mb-6">
-          <label className="text-gray-700 w-full text-xl" htmlFor="user-password">비밀번호</label>
+          <label
+            className="text-gray-700 w-full text-xl"
+            htmlFor="user-nickname"
+          >
+            이름
+          </label>
           <input
             className="text-xl rounded-lg border-2"
-            name="user-password"
+            name="user-nickname"
+            value={name}
+            onChange={onChangeName}
+            required
+          />
+        </div>
+        <div className="flex justify-between mb-6">
+          <label className="text-gray-700 w-full text-xl" htmlFor="user-id">
+            잔액
+          </label>
+          <input
+            className="text-xl rounded-lg border-2"
+            value={balance}
+            onChange={onChangeBalance}
+            required
+          />
+        </div>
+        <div className="flex justify-between mb-6">
+          <label
+            className="text-gray-700 w-full text-xl"
+            htmlFor="user-password"
+          >
+            비밀번호
+          </label>
+          <input
+            className="text-xl rounded-lg border-2"
             value={password}
             type="password"
             onChange={onChangePassword}
             required
           />
         </div>
-        <div className="flex justify-between">
-          <label className="text-gray-700 w-full text-xl"  htmlFor="user-password-check">비밀번호체크</label>
-          <input
-          className="text-xl rounded-lg border-2"
-            name="user-password-check"
-            type="password"
-            value={passwordCheck}
-            required
-            onChange={onChangePasswordCheck}
-          />
-        </div>
-        {passwordError && <div className="mt-3 text-red-700">비밀번호가 일치하지 않습니다. </div>}
-        <button className="mt-11 border rounded bg-slate-700 text-white px-16 py-2" type="submit">가입하기</button>
+        <button
+          className="mt-11 border rounded bg-slate-700 text-white px-16 py-2"
+          type="submit"
+        >
+          가입하기
+        </button>
       </form>
     </div>
   );
