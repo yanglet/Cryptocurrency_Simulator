1fef8cccf9fc49ec848004e3aeb9753c1533788f
yanglet <y2yone023@naver.com>
2022-07-06 00:47:04 +0900

    feat: 게시글, 댓글 API 스펙 변경

--- backend/module-domain/src/main/java/com/project/cs/comment/response/CommentDto.java
+++ backend/module-domain/src/main/java/com/project/cs/comment/response/CommentDto.java
@@ -1,21 +1,30 @@
 package com.project.cs.comment.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.project.cs.comment.entity.Comment;
-import com.project.cs.member.response.MemberDto;
-import com.project.cs.member.response.SimpleMemberDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class CommentDto {
     private String content;
-    private SimpleMemberDto member;
+    private String name;
+    private String email;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime modifiedTime;
 
     public CommentDto(Comment comment) {
         this.content = comment.getContent();
-        this.member = new SimpleMemberDto(comment.getMember());
+        this.name = comment.getMember().getName();
+        this.email = comment.getMember().getEmail();
+        this.createTime = comment.getCreateTime();
+        this.modifiedTime = comment.getModifiedTime();
     }
 }
--- backend/module-domain/src/main/java/com/project/cs/post/response/SimplePostDto.java
+++ backend/module-domain/src/main/java/com/project/cs/post/response/SimplePostDto.java
@@ -1,5 +1,6 @@
 package com.project.cs.post.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.project.cs.comment.response.CommentDto;
 import com.project.cs.member.response.MemberDto;
 import com.project.cs.post.entity.Post;
@@ -8,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -16,12 +18,20 @@ public class SimplePostDto {
     private Long id;
     private String title;
     private String content;
-    private MemberDto member;
+    private String name;
+    private String email;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime modifiedTime;
 
     public SimplePostDto(Post post) {
         this.id = post.getId();
         this.title = post.getTitle();
         this.content = post.getContent();
-        this.member = new MemberDto(post.getMember());
+        this.name = post.getMember().getName();
+        this.email = post.getMember().getEmail();
+        this.createTime = post.getCreateTime();
+        this.modifiedTime = post.getModifiedTime();
     }
 }
