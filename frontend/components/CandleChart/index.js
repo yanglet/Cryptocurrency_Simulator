046252952403570c988c34a3ae101da817f1db0e
sein <hyuntpdls@gmail.com>
2022-08-29 02:18:46 +0900

    design: 캔들스틱차트관련 디자인 수정

--- frontend/components/CandleChart/index.js
+++ frontend/components/CandleChart/index.js
@@ -5,6 +5,7 @@ import Likes from "./Likes";
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 function CandleChart({ tickerId, params }) {
+  console.log(tickerId)
   const id = `${tickerId}` - 1;
 
   let url = "http://localhost:9090/v1/api/cryptocurrencies";
@@ -15,36 +16,37 @@ function CandleChart({ tickerId, params }) {
   if (!data) return <div>loading...</div>;
 
   return (
-    <div>
-      <div className="flex">
-        <p className="text-2xl font-bold">{data[id].korean_name}</p>
-        <p className="my-auto text-sm ml-2">{data[id].market}</p>
-        <div className="my-auto text-center mx-3">
-          <Likes market={data[id].market} />
-        </div>
-      </div>
-      <div className="flex justify-between">
-        <p className="text-4xl font-bold">
-          {data[id].trade_price.toLocaleString()}
-        </p>
-        <div>
-          <div className="flex">
-            <p className="mr-11">고가</p>
-            <p className="mr-11">{data[id].high_price.toLocaleString()}</p>
-            <p className="mr-11">거래량(24H)</p>
-            <p>{data[id].low_price.toLocaleString()}</p>
-          </div>
-          <div className="flex">
-            <p className="mr-11">저가</p>
-            <p className="mr-11">{data[id].low_price.toLocaleString()}</p>
-            <p className="mr-11">거래대금</p>
-            <p className="text-sm">
-              {data[id].acc_trade_price_24h.toLocaleString()}
-            </p>
+    <div className="">
+      <div className="bg-white px-4 py-4 mb-3">
+        <div className="flex">
+          <p className="text-2xl font-bold">{data[id].korean_name}</p>
+          <p className="my-auto text-sm ml-2">{data[id].market}</p>
+          <div className="my-auto text-center mx-3">
+            <Likes market={data[id].market} />
           </div>
         </div>
+        <div className="flex justify-between">
+          <p className="text-4xl font-bold">
+            {data[id].trade_price.toLocaleString()}
+          </p>
+          <div>
+            <div className="flex">
+              <p className="mr-11">고가</p>
+              <p className="mr-11">{data[id].high_price.toLocaleString()}</p>
+              <p className="mr-11">거래량(24H)</p>
+              <p>{data[id].low_price.toLocaleString()}</p>
+            </div>
+            <div className="flex">
+              <p className="mr-11">저가</p>
+              <p className="mr-11">{data[id].low_price.toLocaleString()}</p>
+              <p className="mr-11">거래대금</p>
+              <p className="text-sm">
+                {data[id].acc_trade_price_24h.toLocaleString()}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-
       <Chart params={params} />
     </div>
   );
