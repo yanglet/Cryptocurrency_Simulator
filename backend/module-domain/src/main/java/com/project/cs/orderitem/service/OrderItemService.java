9c43eb9e2fd9568c98d76a70f26830f640b9dfab
yanglet <y2yone023@naver.com>
2022-10-03 00:28:18 +0900

    feat: 보유자산 api 구현

--- backend/module-domain/src/main/java/com/project/cs/orderitem/service/OrderItemService.java
+++ backend/module-domain/src/main/java/com/project/cs/orderitem/service/OrderItemService.java
@@ -1,18 +1,33 @@
 package com.project.cs.orderitem.service;
 
 import com.project.cs.member.entity.Member;
+import com.project.cs.member.repository.MemberRepository;
+import com.project.cs.member.response.MemberDto;
 import com.project.cs.order.entity.Order;
 import com.project.cs.orderitem.entity.OrderItem;
 import com.project.cs.orderitem.repository.OrderItemRepository;
+import com.project.cs.orderitem.response.Holdings;
+import com.project.cs.orderitem.response.OrderItemDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class OrderItemService {
     private final OrderItemRepository orderItemRepository;
+    private final MemberRepository memberRepository;
+
+    public Holdings getOrderItems(Member member){
+        return new Holdings(new MemberDto(memberRepository.findByIdFetch(member.getId())),
+                orderItemRepository.findByMember(member)
+                .stream()
+                .map(OrderItemDto::new)
+                .collect(Collectors.toList()));
+    }
 
     public void deleteOrderItem(Member member, String market){
         OrderItem orderItem = orderItemRepository.findByMemberAndMarket(member, market);
