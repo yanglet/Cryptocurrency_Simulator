902f5b111d2587b6915ebc841dc59ce6a69ba36c
yanglet <y2yone023@naver.com>
2022-10-06 22:48:08 +0900

    feat: 매도, 매수 비즈니스 로직 수정

--- backend/module-domain/src/main/java/com/project/cs/orderitem/entity/OrderItem.java
+++ backend/module-domain/src/main/java/com/project/cs/orderitem/entity/OrderItem.java
@@ -9,6 +9,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
+
+import static com.project.cs.order.service.OrderStatusConstants.TYPE_ASK;
+import static com.project.cs.order.service.OrderStatusConstants.TYPE_BID;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -43,5 +47,23 @@ public class OrderItem extends BaseEntity {
         this.volume = volume;
         this.member = member;
         this.order = order;
+    }
+
+    public void change(String type, Double volume, String price){
+        BigDecimal b1 = new BigDecimal(this.price).multiply(BigDecimal.valueOf(this.volume));
+        BigDecimal b2 = new BigDecimal(price).multiply(BigDecimal.valueOf(volume));
+        BigDecimal add = b1.add(b2);
+        double volAsk = this.volume - volume;
+        double volBid = this.volume + volume;
+        if(TYPE_ASK.equals(type)){ // 매도
+            if(volAsk < 0){
+                throw new IllegalArgumentException();
+            }
+            this.volume = volAsk;
+            this.price = String.valueOf(add.divide(BigDecimal.valueOf(volAsk), 2, BigDecimal.ROUND_CEILING));
+        }else if(TYPE_BID.equals(type)){ // 매수
+            this.volume = volBid;
+            this.price = String.valueOf(add.divide(BigDecimal.valueOf(volBid), 2, BigDecimal.ROUND_CEILING));
+        }
     }
 }
--- backend/module-domain/src/main/java/com/project/cs/orderitem/repository/OrderItemRepositoryCustom.java
+++ backend/module-domain/src/main/java/com/project/cs/orderitem/repository/OrderItemRepositoryCustom.java
@@ -1,10 +1,12 @@
 package com.project.cs.orderitem.repository;
 
 import com.project.cs.member.entity.Member;
+import com.project.cs.order.entity.Order;
 import com.project.cs.orderitem.entity.OrderItem;
 
 import java.util.List;
 
 public interface OrderItemRepositoryCustom {
     List<OrderItem> findByMember(Member member);
+    OrderItem findByOrder(Order order);
 }
