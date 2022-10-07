c4200e848f64c40b20d94eb3ea6bad36c9ba1234
sein <hyuntpdls@gmail.com>
2022-10-07 17:45:50 +0900

    feat: 매수 매도 기능 구현

--- frontend/components/Trading/components/Trading.jsx
+++ frontend/components/Trading/components/Trading.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
-import Form from '../common/Form';
+import React from "react";
+import { useContext } from "react";
+import { MembersContext } from "../../../contexts/Member";
+import Form from "../common/Form";
 
-function Trading({ price }) {
-
-    return (
-        <div className=" bg-white mt-5 ">
-        <div className="font-bold py-2 px-4">주문</div>
-        <div className="grid grid-cols-2 ">
-            <Form price={price} tradingType="매수" tradingPrice="매수가격" btnColor="py-3 px-36 bg-red-600 text-white rounded-lg" /> 
-            <Form price={price} tradingType="매도" tradingPrice="매도가격" btnColor="py-3 px-36 bg-blue-600 text-white rounded-lg" />
-        </div>
+function Trading({ content }) {
+  const member = useContext(MembersContext)
+  console.log("멤버", member.balance)
+  return (
+    <div className=" ">
+      <div className="font-bold py-2 px-4">주문</div>
+      <div className="grid grid-cols-2 ">
+        <Form
+          balance={member.balance}
+          content={content}
+          tradingType="매수"
+          type="bid"
+          ordTypes={[
+            {
+              id: 1,
+              title: "지정가",
+              value: "limit",
+            },
+            {
+              id: 2,
+              title: "시장가",
+              value: "price",
+            },
+          ]}
+          tradingPrice="매수가격"
+          btnColor="py-3 px-36 bg-red-600 text-white rounded-lg"
+        />
+        <Form
+          content={content}
+          tradingType="매도"
+          type="ask"
+          ordTypes={[
+            {
+              id: 1,
+              title: "지정가",
+              value: "limit",
+            },
+            {
+              id: 2,
+              title: "시장가",
+              value: "market",
+            },
+          ]}
+          tradingPrice="매도가격"
+          btnColor="py-3 px-36 bg-blue-600 text-white rounded-lg"
+        />
       </div>
-    );
+    </div>
+  );
 }
 
 export default Trading;
--- frontend/components/Trading/containers/TradingContainer.jsx
+++ frontend/components/Trading/containers/TradingContainer.jsx
@@ -1,22 +1,24 @@
 import React, {useState, useEffect} from 'react';
 import { useContext } from 'react';
 import { CryptocurrencyContext } from '../../../contexts/Cryptocurrency';
+import { MembersProvider } from '../../../contexts/Member';
 import Trading from '../components/Trading';
 
-function TradingContainer({ params, tickerId, setTickerId}) {
+function TradingContainer({ tickerId }) {
     const content = useContext(CryptocurrencyContext);
-    const [price, setPrice] = useState("");
+    const [data, setData] = useState("")
     const id = `${tickerId}` - 1;
 
     useEffect(() => {
-        if(content && content.length > 0){
-           setPrice(`${content[id].trade_price.toLocaleString()}`);
-        }
+        { content[id] && setData(content[id])}
     }, [content, id]);
     
     return (
         <div>
-            <Trading price={price} tickerId={tickerId} />
+            <MembersProvider>
+                <Trading content={data} />
+            </MembersProvider>
+           
         </div>
     );
 }
