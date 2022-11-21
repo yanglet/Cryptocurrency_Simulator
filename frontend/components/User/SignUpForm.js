65403ebbcfe27c4fb79e11249dbd8cd4aa9fdccb
imssein <hyuntpdls@gmail.com>
2022-11-21 12:57:59 +0900

    feat: tradingbot 체크박스 구현

--- frontend/components/User/SignUpForm.js
+++ frontend/components/User/SignUpForm.js
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import AuthService from "../../services/auth.service";
 import useInput from "../../hooks/useInput";
+import { TbRobot } from "react-icons/tb";
 
 function SignUpForm(props) {
   const router = useRouter();
@@ -9,11 +10,22 @@ function SignUpForm(props) {
   const [name, onChangeName] = useInput("");
   const [password, onChangePassword] = useInput("");
   const [balance, onChangeBalance] = useInput("");
+  const [botYn, setBotYn] = useState("");
+
+  const handleBotYn = (e) => {
+    if (e.target.checked) {
+      setBotYn("y");
+      console.log(botYn);
+    } else {
+      setBotYn("n");
+      console.log(botYn);
+    }
+  };
 
   const onSubmitForm = async (e) => {
     e.preventDefault();
     try {
-      await AuthService.register(balance, email, name, password).then(
+      await AuthService.register(balance, email, name, password, botYn).then(
         () => {
           router.replace("/");
         },
@@ -28,7 +40,7 @@ function SignUpForm(props) {
 
   return (
     <div>
-      <form className="text-center" onSubmit={onSubmitForm}>
+      <form className="text-center w-100" onSubmit={onSubmitForm}>
         <div>
           <input
             className="w-1/3 h-14 text-lg rounded-lg border-2 mb-4 px-2"
@@ -67,15 +79,37 @@ function SignUpForm(props) {
             placeholder="비밀번호"
           />
         </div>
-        <button
-          className="mt-9 border-2 bg-blue-500 w-1/3 h-14 rounded-lg text-white font-bold"
-          type="submit"
-        >
-          회원가입
-        </button>
+        <div className="border-2 inline-block w-1/3 h-14 rounded-lg">
+          <div className="flex justify-between">
+          <div className="py-3 text-lg mb-4 px-2 flex">
+            <input
+              className="w-4 rounded-lg"
+              name="botYn"
+              type="checkbox"
+              value={botYn}
+              onClick={(e) => handleBotYn(e)}
+            />
+            <p className="ml-4 text-gray-500">
+              TraidingBot
+            </p>
+          </div>
+          <div className="py-3 pr-2 text-blue-600">
+            <TbRobot size="28" />
+
+          </div>
+          </div>
+          
+        </div>
+        <div>
+          <button
+            className="mt-9 border-2 bg-blue-500 w-1/3 h-14 rounded-lg text-white font-bold"
+            type="submit"
+          >
+            회원가입
+          </button>
+        </div>
       </form>
     </div>
   );
-  
 }
 export default SignUpForm;
